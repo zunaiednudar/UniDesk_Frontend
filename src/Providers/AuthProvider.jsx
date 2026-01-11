d4408cb7edc77b638afc850e6c6ddd66c81855d7
Samioul51 <akmsamioulislam51@gmail.com>
2026-01-11 18:18:32 +0600

    Authenticated User's Data Retrieval function and update user function added in AuthProvider Wrapper

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -1,6 +1,6 @@
-import React, {createContext, useState} from 'react';
+import React, {createContext, useEffect, useState} from 'react';
 import {auth} from "../Firebase/firebase.init.js";
-import {signInWithEmailAndPassword, createUserWithEmailAndPassword,signOut,GoogleAuthProvider,signInWithPopup } from "firebase/auth";
+import {signInWithEmailAndPassword, createUserWithEmailAndPassword,signOut,GoogleAuthProvider,signInWithPopup,updateProfile,onAuthStateChanged } from "firebase/auth";
 
 export const AuthContext = createContext();
 
@@ -8,22 +8,23 @@ const googleProvider=new GoogleAuthProvider();
 
 const AuthProvider = ({children}) => {
     const [user, setUser] = useState(null);
+    const [loading,setLoading]=useState(false);
 
     // Sign Up
 
     const signUp = (email, password) => {
         return createUserWithEmailAndPassword(auth, email, password);
-    }
+    };
 
     // Login
 
     const login = (email, password) => {
         return signInWithEmailAndPassword(auth, email, password);
-    }
+    };
 
     const logout=()=>{
         return signOut(auth);
-    }
+    };
 
     // Google Authentication
 
@@ -33,14 +34,36 @@ const AuthProvider = ({children}) => {
         } catch(error){
             throw error;
         }
-    }
+    };
+
+    // Update User Profile
+
+    const updateUser=(updatedData)=>{
+        return updateProfile(auth.currentUser,updatedData);
+    };
+
+    // User Data
+
+    useEffect(() => {
+        const unsubscribe=onAuthStateChanged(auth,async (currentUser)=>{
+            setUser(currentUser);
+        });
+        return ()=>unsubscribe();
+    }, []);
+
 
     // Authentication Data
 
     const authData={
         signUp,
         login,
-        logout
+        logout,
+        signInWithGoogle,
+        updateUser,
+        user,
+        setUser,
+        loading,
+        setLoading
     };
 
     return <AuthContext value={authData}>{children}</AuthContext>
