e1b81eee6f5edd10488c60fd753e6281a0df59f1
Samioul51 <akmsamioulislam51@gmail.com>
2026-01-16 00:31:56 +0600

    Navbar of Root Layout implemented

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -14,7 +14,7 @@ export const router = createBrowserRouter([
                 Component: Home
             },
             {
-                path:"/",
+                path:"/home",
                 Component:Home
             },
             {
