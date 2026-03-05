90b9c63aff2f57f99ebbc2e44e5db3be67a18be4
zunaiednudar <zunaied4821@gmail.com>
2026-03-05 10:47:11 +0600

    feat(home): Login.jsx fonts updated

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -63,7 +63,7 @@ const Login = () => {
     }
 
     return (
-        <div className="w-full max-w-full flex inter">
+        <div className="gilroy w-full max-w-full flex">
             {/* Interactive Background */}
             <div className="hidden lg:flex lg:w-full lg:max-w-[50%] bg-gradient-to-br from-[#1E40AF] via-[#1E3A8A] to-[#3B82F6]" style={{ width: '1080px', minHeight: '100vh', position: 'relative' }}>
                 <DotGrid
@@ -81,7 +81,7 @@ const Login = () => {
                 />
 
                 <div className="absolute h-auto inset-0 z-50 flex flex-col mt-50 items-center gap-5">
-                    <div className="w-[70%] text-5xl font-extrabold playfair text-white min-h-25">
+                    <div className="w-[70%] text-5xl font-semibold graphik text-white min-h-25">
                         <TextType
                             text={"Welcome Back to Your Digital Campus"}
                             typingSpeed={100}
@@ -101,7 +101,7 @@ const Login = () => {
             {/*Login Form*/}
 
             <div className="w-full max-w-full lg:max-w-[50%] min-h-screen flex flex-col items-center justify-center px-10">
-                <p className="playfair font-extrabold text-black text-3xl md:text-5xl mb-5">Sign In</p>
+                <p className="graphik font-semibold text-black text-3xl md:text-4xl mb-5">Sign In</p>
                 <p className="text-gray-400 mb-10 text-sm md:text-[16px]">Please enter your university credentials</p>
                 <button onClick={() => handleGoogleLogin(signInWithGoogle, removeUser, logout, navigate, setUserData)} className="w-full max-w-[500px] h-12 btn bg-white text-black border-[#e5e5e5] mb-5 cursor-pointer">
                     <svg aria-label="Google logo" width="16" height="16" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><g><path d="m0 0H512V512H0" fill="#fff"></path><path fill="#34a853" d="M153 292c30 82 118 95 171 60h62v48A192 192 0 0190 341"></path><path fill="#4285f4" d="m386 400a140 175 0 0053-179H260v74h102q-7 37-38 57"></path><path fill="#fbbc02" d="m90 341a208 200 0 010-171l63 49q-12 37 0 73"></path><path fill="#ea4335" d="m153 219c22-69 116-109 179-50l55-54c-78-75-230-72-297 55"></path></g></svg>
@@ -134,7 +134,7 @@ const Login = () => {
 
                     {/* Submit Button */}
 
-                    <button type="submit" className="btn w-full h-12 bg-blue-700 font-bold text-white cursor-pointer text-lg hover:bg-blue-900 ease-in-out duration-600">Sign In</button>
+                    <button type="submit" className="btn w-full h-12 bg-blue-700 font-medium text-white cursor-pointer text-lg hover:bg-blue-900 ease-in-out duration-600">Sign In</button>
                 </form>
                 <div className="text-sm">Don't have an account? <Link to="/signup" className="text-blue-500 font-medium">Create Account</Link></div>
             </div>
@@ -147,7 +147,7 @@ const Login = () => {
                     <div className="modal-action">
                         <form onSubmit={handleForgotPassword} className='w-full'>
                             <fieldset className="fieldset mb-5">
-                                <legend className="fieldset-legend">Enter your email</legend>
+                                <legend className="fieldset-legend font-medium text-sm">Enter your email</legend>
                                 <input type="email" name="forgotEmail" className="input w-full" placeholder="email@stud.kuet.ac.bd" required />
                             </fieldset>
                             <div className='flex gap-5 justify-end'>
