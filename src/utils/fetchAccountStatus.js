c3380d1445042128181fc71e84782729e7295482
Samioul51 <akmsamioulislam51@gmail.com>
2026-04-23 21:28:44 +0600

    feat: add account status fetch function

---
+++ src/utils/fetchAccountStatus.js
@@ -0,0 +1,11 @@
+import axiosSecure from "./axiosSecure.js"
+
+export const fetchAccountStatus=async()=>{
+    try {
+        const res=await axiosSecure.get("/users/account-status");
+
+        return res.data?.user || null;
+    } catch (error) {
+        return null;
+    }
+};
